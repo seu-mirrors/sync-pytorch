64112f7a0c860111c14fc93006e3a34cfd4500a0
TonyWu <213240058@seu.edu.cn>
2025-11-25 14:13:05 +0800

    fix(main.py): 由于pytorch目录结构变更修改逻辑以适配新的目录结构

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import threading
 from glob import glob
 from requests.adapters import HTTPAdapter
 import traceback
+import logging
 
 SHOW_PROGRESS = False
 if "--show-progress" in sys.argv and __name__ == "__main__":
@@ -22,11 +23,12 @@ if SHOW_PROGRESS:
         print("tqdm not found")
         SHOW_PROGRESS = False
 
-base_path = os.path.abspath(os.getenv("TUNASYNC_WORKING_DIR", default = "./sync_dir/")) #同步路径
+base_path = os.path.abspath(os.getenv("TUNASYNC_WORKING_DIR", default = "sync_dir")) #同步路径
 if base_path[-1] != "/":
     base_path += "/"
 base_url = "https://download.pytorch.org/"
 compute_platforms = ["cpu-cxx11-abi", "cpu_pypi_pkg"]
+# ['cpu-cxx11-abi', 'cpu_pypi_pkg', 'cpu', 'cu126', 'cu128', 'cu130', 'rocm6.4']
 threads_count = 16 #线程数量
 user_agent = "Mozilla/5.0 (compatible; sync-pytorch/0.1; +https://github.com/seu-mirrors/sync-pytorch)"
 
@@ -37,7 +39,6 @@ re_pattern = re.compile(r"<a href=\"(\S*)\".*>(\S*)</a>")
 fetch_list = []
 search_metadata_list = []
 fetch_list_lock = threading.Lock()
-whl_set_lock = threading.Lock()
 pkglist = os.path.join(base_path, "packagelist.txt")
 
 session = requests.Session()
@@ -46,6 +47,14 @@ session.mount('https://', HTTPAdapter(max_retries=10))
 session.headers.update({"User-Agent": user_agent})
 
 truncate = lambda path: open(path, "w").close()
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt = "%Y-%m-%d %H:%M:%S",
+    filename = os.path.join(base_path, 'script.log'),
+    filemode = "w"
+)
 
 class search_metadata_thread(threading.Thread):
     def __init__(self, thread_index, index_begin, index_end):
@@ -65,86 +74,12 @@ class search_metadata_thread(threading.Thread):
                 if session.head(search_metadata_list[i]["url"]).status_code == 200:
                     self.fetch_list.append(search_metadata_list[i])
             except Exception as err:
-                print("network error")
-                print(err)
-                traceback.print_exc()
+                logging.exception("network error")
+                logging.error(traceback.format_exc())
                 os._exit(1)
 
         with fetch_list_lock:
             fetch_list.extend(self.fetch_list)
-
-class update_package_thread(threading.Thread):
-    def __init__(self, thread_index, package_info_list, index_begin, index_end, local_dir):
-        threading.Thread.__init__(self)
-        self.thread_index = thread_index
-        self.package_info_list = package_info_list
-        self.index_begin = index_begin
-        self.index_end = index_end
-        self.local_dir = local_dir
-    def run(self):
-        rng = None
-        if SHOW_PROGRESS:
-            rng = tqdm(range(self.index_begin, self.index_end), desc = f"thread #{self.thread_index}", leave = False)
-        else:
-            rng = range(self.index_begin, self.index_end)
-        self.fetch_list = []
-        self.search_metadata_list = []
-        local_dir = self.local_dir
-        for index in rng:
-            package_info = self.package_info_list[index]
-            package_url = package_info["url"]
-            try:
-                package_response = session.get(package_url)
-                if package_response.status_code == 200:
-                    package_html = package_response.text
-                    os.makedirs(os.path.join(local_dir, package_info["name"]), 0o755, True)
-                    with open(os.path.join(local_dir, package_info["name"], "index.html"), "w") as fhandle:
-                        fhandle.write(package_html.replace("href=\"/whl", "href=\"https://mirrors.seu.edu.cn/pytorch/whl"))
-                    res = re_pattern.search(package_html, 0)
-                    while res:
-                        next_pos = res.span(0)[1]
-                        next_res = re_pattern.search(package_html, next_pos)
-                        whl_name = unquote(res.group(1))
-                        if not whl_name.startswith("/"):
-                            # not a wheel, just skip over
-                            print("unknown whl_name: " + whl_name)
-                            res = next_res
-                            continue
-                        # strip leading slash
-                        whl_name = whl_name[1:]
-                        if not whl_name in is_whl_processed:
-                            with whl_set_lock:
-                                is_whl_processed.add(whl_name)
-                            whl_url = urljoin(base_url, res.group(1))
-                            sha256 = None
-                            if "#" in whl_name:
-                                split = whl_name.split("#sha256=")
-                                whl_name = split[0]
-                                sha256 = split[1]
-                            assert whl_name.endswith(".whl") or whl_name.endswith(".tar.gz")
-                            self.fetch_list.append({
-                                "name" : whl_name,
-                                "url" : whl_url,
-                                "local_path" : os.path.join(base_path, whl_name),
-                                "sha256" : sha256
-                            })
-                            self.search_metadata_list.append({
-                                "name" : whl_name + ".metadata",
-                                "url" : whl_url.replace(".whl", ".whl.metadata").replace(".tar.gz", ".tar.gz.metadata"),
-                                "local_path" : os.path.join(base_path, whl_name + ".metadata"),
-                            })
-                        res = next_res
-                else:
-                    print(package_info["name"] + " network error " + str(package_response.status_code))
-            except Exception as err:
-                print("network error")
-                print(err)
-                traceback.print_exc()
-                os._exit(1)
-        
-        with fetch_list_lock:
-            fetch_list.extend(self.fetch_list)
-            search_metadata_list.extend(self.search_metadata_list)
 
 def load_existed_files():
     global existed_files
@@ -154,61 +89,97 @@ def load_existed_files():
             existed_files = pickle.load(fhandle)
 
 def update_index(platform = ""):
+    logging.info(f"current platform = {platform}")
     os.makedirs(os.path.join(base_path, "whl"), 0o755, True)
     local_dir = os.path.join(base_path, "whl", platform, "simple")
 
     url = f"{base_url}whl/"
     if platform != "":
         url += platform + "/"
+    search_package_recursive(url, local_dir)
+
+def search_package_recursive(url, local_dir):
+    logging.info(f"current url = {url} local_dir = {local_dir}")
     try:
-        projects_list_response = session.get(url)
-        if projects_list_response.status_code == 200:
-            projects_list_html = projects_list_response.text
+        response = session.get(url)
+        if response.status_code == 200:
+            html_content = response.text
             os.makedirs(local_dir, 0o755, True)
             with open(os.path.join(local_dir, "index.html"), "w") as fhandle:
-                fhandle.write(projects_list_html)
+                fhandle.write(html_content.replace("href=\"/whl", "href=\"https://mirrors.seu.edu.cn/pytorch/whl"))
+        # 搜索包或者whl
+        search_pos = 0
+        res = re_pattern.search(html_content, search_pos)
+        while res:
+            search_pos = res.span(0)[1]
+            next_res = re_pattern.search(html_content, search_pos)
+            # res.group(1) <-> url
+            # eg.
+            # certifi/
+            # /whl/certifi-2022.12.7-py3-none-any.whl#sha256=4ad3232f5e926d6718ec31cfc1fcadfde020920e278684144551c91769c7bc18
+            
+            # res.group(2) <-> name
+            # eg.
+            # certifi
+            # certifi-2022.12.7-py3-none-any.whl
 
-            # 获取包列表
-            print("fetch package list")
-            package_info_list = []
-            search_pos = 0
-            res = re_pattern.search(projects_list_html, search_pos)
-            while res:
-                # 包信息
-                package_url = url + res.group(1)
-                package_name = res.group(2)
-                package_info_list.append({"url" : package_url, "name" : package_name})
-                # 更新搜索条件
-                search_pos = res.span(0)[1]
-                res = re_pattern.search(projects_list_html, search_pos)
-            package_counts = len(package_info_list)
-            print(f"platform: {platform}    " + "package counts: " + str(package_counts))
-
-            print("fetch file list")
-            threads = []
-            search_metadata_list.clear()
-            package_per_thread = package_counts // threads_count
-            for i in range(0, threads_count, 1):
-                thread = update_package_thread(i, package_info_list, i * package_per_thread, (i + 1) * package_per_thread if i != threads_count - 1 else package_counts, local_dir)
-                threads.append(thread)
-                thread.start()
-            for t in threads:
-                t.join()
-
-            print("fetch file metadata")
-            threads.clear()
-            search_metadata_per_thread = len(search_metadata_list) // threads_count
-            for i in range(0, threads_count, 1):
-                thread = search_metadata_thread(i, i * search_metadata_per_thread, (i + 1) * search_metadata_per_thread if i != threads_count - 1 else len(search_metadata_list))
-                threads.append(thread)
-                thread.start()
-            for t in threads:
-                t.join()
+            # 过滤url
+            # cpu*
+            # cu*
+            # rocm*
+            pattern_str = r'^(cpu|cu|rocm)\S*$'
+            item_url = unquote(res.group(1))
+            item_name = res.group(2)
+            if re.match(pattern_str, item_url):
+                res = next_res
+                logging.info(f"skip item_url = {item_url}")
+                continue
+            if item_url.startswith("/"):
+                # whl or archive
+                whl_name = item_name
+                if whl_name in is_whl_processed:
+                    logging.debug(f"skip processed whl_name = {whl_name}")
+                    res = next_res
+                    continue
+                is_whl_processed.add(whl_name)
+                whl_url = urljoin(base_url, item_url)
+                sha256 = None
+                if "#" in whl_url:
+                    split = whl_url.split("#sha256=")
+                    whl_url = split[0]
+                    sha256 = split[1]
+                assert whl_name.endswith(".whl") or whl_name.endswith(".tar.gz"), f"unexpected extension name (whl_name = ${whl_name})"
+                fetch_list.append({
+                    "name" : whl_name,
+                    "url" : whl_url,
+                    "local_path" : os.path.join(base_path, whl_name),
+                    "sha256" : sha256
+                })
+                logging.debug(f"fetch_info = {fetch_list[-1]}")
+                search_metadata_list.append({
+                    "name" : whl_name + ".metadata",
+                    "url" : whl_url.replace(".whl", ".whl.metadata").replace(".tar.gz", ".tar.gz.metadata"),
+                    "local_path" : os.path.join(base_path, whl_name + ".metadata")
+                })
+                logging.debug(f"search_metadata_info = {search_metadata_list[-1]}")
+            else:
+                # dir
+                search_package_recursive(urljoin(url, item_url), os.path.join(local_dir, item_url))
+            res = next_res
     except Exception as err:
-        print("error")
-        print(err)
-        traceback.print_exc()
+        logging.exception("exception occurred")
+        logging.error(traceback.format_exc(err))
         os._exit(1)
+
+def search_metadata():
+    threads = []
+    search_metadata_per_thread = len(search_metadata_list) // threads_count
+    for i in range(0, threads_count, 1):
+        thread = search_metadata_thread(i, i * search_metadata_per_thread, (i + 1) * search_metadata_per_thread if i != threads_count - 1 else len(search_metadata_list))
+        threads.append(thread)
+        thread.start()
+    for t in threads:
+        t.join()
 
 def get_platforms():
     response = session.get("https://raw.githubusercontent.com/pytorch/pytorch.github.io/refs/heads/site/assets/quick-start-module.js")
@@ -224,9 +195,8 @@ def get_platforms():
                 else:
                     compute_platforms.append(info[0] + info[1])
         except Exception as err:
-            print("failed to parse platform info")
-            print(err)
-            traceback.print_exc()
+            logging.exception("failed to parse platform info")
+            logging.error(traceback.format_exc())
         
 def update_human_index():
     os.makedirs(os.path.join(base_path, "whl"), 0o755, True)
@@ -273,6 +243,7 @@ def remove_outdated_files():
             outdated_files.append(existed_file_path)
     for path in outdated_files:
         os.remove(path)
+        logging.info(f"remove file: {path}")
 
 def export_aria2c():
     with open(pkglist, "w") as fhandle:
@@ -317,8 +288,10 @@ def main():
     get_platforms()
     update_human_index()
     load_existed_files()
+    logging.info(compute_platforms)
     for platform in compute_platforms:
         update_index(platform)
+    search_metadata()
     remove_outdated_files()
     export_aria2c()
     perform_download()
